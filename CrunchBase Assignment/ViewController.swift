f651200c4d52ac09778a618d00818471224c2218
primekrieger <primekrieger@gmail.com>
2015-11-01 17:34:05 +0530

    Initial Commit

---
+++ CrunchBase Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrunchBase Assignment
+//
+//  Created by Diwakar Kamboj on 01/11/15.
+//  Copyright © 2015 Nagarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
