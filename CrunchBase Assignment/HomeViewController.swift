0ff97040861a32eebd7c75dafddfa461b75a9193
primekrieger <primekrieger@gmail.com>
2015-11-02 02:27:34 +0530

    v1

--- CrunchBase Assignment/HomeViewController.swift
+++ CrunchBase Assignment/HomeViewController.swift
@@ -66,6 +66,9 @@ class HomeViewController: UIViewController, UIPickerViewDataSource, UIPickerView
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
+        
+        
+        
         let destController = segue.destinationViewController as! ListTableViewController
         
         destController.queryType = queryType
--- CrunchBase Assignment/ListTableViewController.swift
+++ CrunchBase Assignment/ListTableViewController.swift
@@ -11,12 +11,14 @@ import Alamofire
 
 class ListTableViewController: UITableViewController {
 
+    @IBOutlet weak var loadActivityIndicator: UIActivityIndicatorView!
     
     var queryType = ""
     var query = ""
     var numResults = 0
     
-    var op: [String] = []
+    var cellText: [String] = []
+//    var cellSub: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,9 @@ class ListTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        //loadActivityIndicator.center = CGPointMake(UIScreen.mainScreen().bounds.size.width/2,UIScreen.mainScreen().bounds.size.height/2)
+        
         
         let url = "https://api.crunchbase.com/v/3/" + queryType
         
@@ -38,23 +43,94 @@ class ListTableViewController: UITableViewController {
                 //                print(response.data)     // server data
                 print("Status: \(response.result)")   // result of response serialization
                 
-                if let num = response.result.value?.objectForKey("data")?.objectForKey("paging")?.objectForKey("total_items") as? NSNumber {
+                if (response.result.description == "FAILURE") {
                     
-                    self.numResults = num.integerValue
+                    self.showAlerts("Error!", alertMessage: "Something went wrong")
                     
-                    for i in 0..<num.integerValue {
-                        let JSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(i).objectForKey("properties")?.objectForKey("name")
-                        //print("JSON: \(JSON!)")
-                        self.op.append(JSON as! String)
+                }
+                
+                else {
+                
+                let num = response.result.value?.objectForKey("data")?.objectForKey("paging")?.objectForKey("total_items") as? NSNumber
+                
+                self.numResults = num!.integerValue
+                
+                    if self.numResults == 0 {
+                        self.showAlerts("Oops!", alertMessage: "Your query didn't return any results")
+                    }
+                    
+                    else {
+                        if self.numResults > 99 {
+                            self.numResults = 100
+                        }
+                    
+                        //print(self.numResults)
+                        
+                        if self.queryType == "organizations" {
+                            for i in 0..<self.numResults {
+                                let nameJSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(i).objectForKey("properties")?.objectForKey("name")
+//                                let roleJSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(i).objectForKey("properties")?.objectForKey("primary_role")
+                                //print("JSON: \(JSON!)")
+                                self.cellText.append(nameJSON as! String)
+//                                self.cellSub.append(roleJSON as! String)
+                            }
+                        }
+                        
+                        else if self.queryType == "people" {
+                            for i in 0..<self.numResults {
+                                let firstNameJSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(i).objectForKey("properties")?.objectForKey("first_name")
+                                let lastNameJSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(i).objectForKey("properties")?.objectForKey("last_name")
+//                                let organizationNameJSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(i).objectForKey("properties")?.objectForKey("title")
+                                
+                                var fullName = firstNameJSON as! String + " "
+                                fullName += lastNameJSON as! String
+                                
+                                self.cellText.append(fullName)
+                                
+                              
+//                                self.cellSub.append(organizationNameJSON as! String)
+                                
+                            }
+                        }
+                        
+                        else {
+                            for i in 0..<self.numResults {
+                                let nameJSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(i).objectForKey("properties")?.objectForKey("name")
+                                self.cellText.append(nameJSON as! String)
+                            }
+                        }
                     }
                 }
-                //
-                //                if let JSON = response.result.value?.objectForKey("data")?.objectForKey("items")?.objectAtIndex(0).objectForKey("properties")?.objectForKey("name") {
-                //                    
-                //                }
                 
+                self.loadActivityIndicator.stopAnimating()
                 self.tableView.reloadData()
         }
+    }
+    
+    func showAlerts(alertTitle: String, alertMessage: String) {
+        
+        let alertController = UIAlertController(
+            title: alertTitle,
+            message: alertMessage,
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        
+        //                    let cancelAction = UIAlertAction(
+        //                        title: "Cancel",
+        //                        style: UIAlertActionStyle.Destructive) { (action) in
+        //                            // ...
+        //                    }
+        
+        let confirmAction = UIAlertAction(
+            title: "OK", style: UIAlertActionStyle.Default) { (action) in
+                // ...
+        }
+        
+        alertController.addAction(confirmAction)
+        //alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,6 +147,13 @@ class ListTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+//        
+//        if numResults >= 100 {
+//            numResults = 100
+//        }
+//        
+//        print(numResults)
+        
         return numResults
     }
 
@@ -79,7 +162,8 @@ class ListTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = op[indexPath.row]
+        cell.textLabel!.text = cellText[indexPath.row]
+//        cell.detailTextLabel!.text = cellSub[indexPath.row]
 
         return cell
     }
